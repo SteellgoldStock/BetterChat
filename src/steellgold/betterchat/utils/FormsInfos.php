42a4dad0b6e83d31e935ad473dddadb5de17ee4e
Gaëtan H <steellgold0@gmail.com>
2022-12-18 16:57:26 +0100

    Couleurs

---
+++ src/steellgold/betterchat/utils/FormsInfos.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace steellgold\betterchat\utils;
+
+class FormsInfos {
+	public static $colors = [
+		"§4Rouge foncé",
+		"§cRouge",
+		"§6Or",
+		"§eJaune",
+		"§2Vert foncé",
+		"§aVert",
+		"§bCyan",
+		"§3Cyan foncé",
+		"§1Bleu foncé",
+		"§9Bleu",
+		"§dRose",
+		"§5Rose foncé",
+		"§fBlanc",
+		"§7Gris",
+		"§8Gris foncé",
+		"§0Noir"
+	];
+}
