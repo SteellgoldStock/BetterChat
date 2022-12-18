07baa7fbee8b958a9d876b94997ea15fe0249bf2
Gaëtan H <steellgold0@gmail.com>
2022-12-18 16:58:40 +0100

    Gestionnaire de grades

---
+++ src/steellgold/betterchat/player/ranks/RankManager.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace steellgold\betterchat\player\ranks;
+
+use steellgold\betterchat\utils\MySQL;
+
+class RankManager {
+
+	/** @var Rank[] $ranks */
+	private static array $ranks = [];
+	public function __construct() {
+		$this->loadRanks();
+	}
+
+	private function loadRanks() {
+		$data = MySQL::mysqli()->query("SELECT * FROM ranks")->fetch_assoc();
+		var_dump($data);
+	}
+
+	public static function addRank(Rank $rank): void {
+		self::$ranks[$rank->getUuid()] = $rank;
+	}
+
+	public static function removeRank(string $uuid): void {
+		unset(self::$ranks[$uuid]);
+	}
+
+	public static function getRank(string $uuid): ?Rank {
+		return self::$ranks[$uuid] ?? null;
+	}
+
+	public static function getRanks(): array {
+		return self::$ranks;
+	}
+
+	public static function getRankByIdentifier(string $identifier): ?Rank {
+		foreach (self::$ranks as $rank) {
+			if ($rank->getRankIdentifier() === $identifier) {
+				return $rank;
+			}
+		}
+		return null;
+	}
+}
