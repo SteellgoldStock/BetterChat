1cdd66f5305fb51ac39dc7234dc0387dd2c7270a
Gaëtan H <steellgold0@gmail.com>
2022-12-19 01:30:30 +0100

    Récupérations des plugins par commandes (Tests)

--- src/steellgold/betterchat/BetterChat.php
+++ src/steellgold/betterchat/BetterChat.php
@@ -5,13 +5,16 @@ namespace steellgold\betterchat;
 use CortexPE\Commando\exception\HookAlreadyRegistered;
 use CortexPE\Commando\PacketHooker;
 use pocketmine\plugin\PluginBase;
+use pocketmine\Server;
 use steellgold\betterchat\commands\RankCommand;
 use steellgold\betterchat\listeners\PlayerListeners;
 use steellgold\betterchat\player\ranks\RankManager;
 use steellgold\betterchat\utils\MySQL;
 
 class BetterChat extends PluginBase {
+
 	public static BetterChat $instance;
+
 	public RankManager $rankManager;
 
 	const BETTERCHAT_PREFIX = "§f[§gBetterChat§f]§r ";
@@ -22,7 +25,7 @@ class BetterChat extends PluginBase {
 	 */
 	protected function onEnable(): void {
 		self::$instance = $this;
-		if(!PacketHooker::isRegistered()) {
+		if (!PacketHooker::isRegistered()) {
 			PacketHooker::register($this);
 		}
 
@@ -32,6 +35,30 @@ class BetterChat extends PluginBase {
 		$this->rankManager = new RankManager();
 		$this->getServer()->getCommandMap()->register("betterchat", new RankCommand($this, "rank", "Manage ranks"));
 		$this->getServer()->getPluginManager()->registerEvents(new PlayerListeners(), $this);
+
+		$plugins = [];
+		foreach (Server::getInstance()->getPluginManager()->getPlugins() as $plugin) {
+			$plugins[$plugin->getName()] = [
+				"count" => 0,
+				"commands" => []
+			];
+		}
+		$plugins["PocketMine-MP"] = [
+			"count" => 0,
+			"commands" => []
+		];
+
+		foreach (Server::getInstance()->getCommandMap()->getCommands() as $command) {
+			if (Server::getInstance()->getPluginCommand($command->getName()) == null) {
+				$plugins["PocketMine-MP"]["count"]++;
+				$plugins["PocketMine-MP"]["commands"][] = $command->getName();
+			} else {
+				$plugins[Server::getInstance()->getPluginCommand($command->getName())->getOwningPlugin()->getName()]["count"]++;
+				$plugins[Server::getInstance()->getPluginCommand($command->getName())->getOwningPlugin()->getName()]["commands"][] = $command->getName();
+			}
+		}
+
+		var_dump($plugins);
 	}
 
 	public static function getInstance(): BetterChat {
