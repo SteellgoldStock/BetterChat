7790af26760e1a9c96e85e259e5485a2b5034f52
Gaëtan H <steellgold0@gmail.com>
2022-12-19 01:30:43 +0100

    Update PlayerListeners.php

--- src/steellgold/betterchat/listeners/PlayerListeners.php
+++ src/steellgold/betterchat/listeners/PlayerListeners.php
@@ -4,6 +4,8 @@ namespace steellgold\betterchat\listeners;
 
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\event\server\CommandEvent;
+use steellgold\betterchat\BetterChat;
 use steellgold\betterchat\player\Session;
 
 class PlayerListeners implements Listener {
